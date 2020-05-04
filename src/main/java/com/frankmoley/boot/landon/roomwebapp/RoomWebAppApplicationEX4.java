7af49a46dab33cb3bf833e4d4859d953148a04fb
EMAD-PC\Emad <eabdelhamid@ms3-inc.com>
2020-05-04 00:16:26 -0400

    Initial commit

---
+++ src/main/java/com/frankmoley/boot/landon/roomwebapp/RoomWebAppApplicationEX4.java
@@ -0,0 +1,12 @@
+package com.frankmoley.boot.landon.roomwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebAppApplicationEX4 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebAppApplicationEX4.class, args);
+	}
+}
